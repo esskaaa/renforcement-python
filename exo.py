10565aa0f9ae3cbd74f131e67397fca07c6fc17c
unknown <bnugier@gamingcampus.local>
2021-03-30 14:49:00 +0200

    Exo 1

---
+++ exo.py
@@ -0,0 +1,15 @@
+import math
+
+# Cours 1 (1
+
+temps = 6.892 
+distance = 19.7
+vitesse = distance/temps
+print (round(vitesse, 1))
+
+# Cours 1 (2
+
+nom = input('Quel est votre nom ? ')
+age = input('Quel est votre age ? ')
+print(nom, age)
+
